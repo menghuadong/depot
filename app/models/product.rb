a0fcc9656b213f4e046577e2aa81ce0696572667
menghuadong <huadongmeng@gmail.com>
2013-01-04 12:16:35 +0800

    add_cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,22 @@
 class Product < ActiveRecord::Base
 
   default_scope :order => 'title'
+  has_many:line_items
   validates:title,:description,:image_url,:presence=>true
   validates:price,:numericality=>{:greater_than_or_equal_to=>0.01}
   validates:title,:uniqueness=>true
   validates:image_url,:format=>{
 	:with => %r{\.(gif|jpg|png)}i,
-	:message=>'must be a RL for GIF,JPG or PNG image.'
-}
+	:message=>'must be a RL for GIF,JPG or PNG image.'}
+
+before_destroy:ensure_not_referenced_by_any_line_item
+  private
+  def ensure_not_referenced_by_any_line_item
+	if line_items.empty?
+	 return true
+	else
+	 errors.add(:base,'line items present')
+	 return false
+  	end
+  end
 end
